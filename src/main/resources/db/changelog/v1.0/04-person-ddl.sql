4e972549755872084cff1a2bebad8d43832d6bbc
an.i.chernova <an.i.chernova@tinkoff.ru>
2024-10-13 15:35:29 +0300

    feat: added basic db tables

--- src/main/resources/db/changelog/v1.0/04-person-ddl.sql
+++ src/main/resources/db/changelog/v1.0/04-person-ddl.sql
@@ -3,6 +3,5 @@ CREATE TABLE IF NOT EXISTS person
     id UUID DEFAULT uuid() PRIMARY KEY,
     first_name VARCHAR(20) NOT NULL,
     last_name VARCHAR(20) NOT NULL,
-    middle_name VARCHAR(20),
-    is_alive BOOLEAN NOT NULL DEFAULT TRUE
+    middle_name VARCHAR(20)
 );
