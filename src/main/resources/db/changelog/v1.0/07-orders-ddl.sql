b7a4a0fd72d71e46d50ea285015fc829b5631d9f
an.i.chernova <an.i.chernova@tinkoff.ru>
2024-10-13 15:49:11 +0300

    feat: added entity models

--- src/main/resources/db/changelog/v1.0/07-orders-ddl.sql
+++ src/main/resources/db/changelog/v1.0/07-orders-ddl.sql
@@ -2,8 +2,7 @@ CREATE TABLE IF NOT EXISTS orders
 (
     id UUID DEFAULT uuid() PRIMARY KEY,
     status order_status NOT NULL,
-    daily_norm BIGINT NOT NULL,
-    dayCount BIGINT NOT NULL,
+    size BIGINT NOT NULL,
     person_id UUID NOT NULL REFERENCES person,
     department_id UUID NOT NULL REFERENCES department,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
