98edb469437ca7c04650eb544a0b5debd635137f
an.i.chernova <an.i.chernova@tinkoff.ru>
2024-10-21 22:22:59 +0300

    tests: added controller tests

--- src/test/kotlin/ru/itmo/highload_systems/TestHighloadSystemsApplication.kt
+++ src/test/kotlin/ru/itmo/highload_systems/TestHighloadSystemsApplication.kt
@@ -2,9 +2,9 @@ package ru.itmo.highload_systems
 
 import org.springframework.boot.fromApplication
 import org.springframework.boot.with
-import ru.itmo.highload_systems.common.config.TestcontainersConfiguration
+import ru.itmo.highload_systems.common.config.TestContainersConfiguration
 
 
 fun main(args: Array<String>) {
-    fromApplication<HighloadSystemsApplication>().with(TestcontainersConfiguration::class).run(*args)
+    fromApplication<HighloadSystemsApplication>().with(TestContainersConfiguration::class).run(*args)
 }
