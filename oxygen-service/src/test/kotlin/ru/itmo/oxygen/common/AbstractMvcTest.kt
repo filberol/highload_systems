7e930a21abe60c9f94c9dc97c2553b26de5abc90
Vadim Mikhu <mkh.vadim@gmail.com>
2024-11-10 20:11:14 +0300

    fix: Fix test deps

--- oxygen-service/src/test/kotlin/ru/itmo/oxygen/common/AbstractMvcTest.kt
+++ oxygen-service/src/test/kotlin/ru/itmo/oxygen/common/AbstractMvcTest.kt
@@ -1,22 +1,20 @@
 package ru.itmo.highload_systems.common
 
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration
 import org.springframework.context.annotation.Import
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper
-import ru.itmo.highload_systems.HighloadSystemsApplication
+import ru.itmo.oxygen.OxygenServiceApplication
 
 
-@ContextConfiguration(classes = [HighloadSystemsApplication::class])
+@ContextConfiguration(classes = [OxygenServiceApplication::class])
 @Import(
     value = [
         ObjectMapper::class
     ]
 )
+@Suppress("SpringJavaInjectionPointsAutowiringInspection")
 abstract class AbstractMvcTest {
     @Autowired
     protected lateinit var mockMvc: MockMvc
