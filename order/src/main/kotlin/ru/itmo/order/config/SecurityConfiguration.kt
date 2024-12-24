f37ecb2bdcb3c98a66308f08f02540959f8abc34
anekoss <ann1974chernova@gmail.com>
2024-12-24 15:29:29 +0300

    feat: added swagger to order service

--- order/src/main/kotlin/ru/itmo/order/config/SecurityConfiguration.kt
+++ order/src/main/kotlin/ru/itmo/order/config/SecurityConfiguration.kt
@@ -25,13 +25,6 @@ class SecurityConfiguration(
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http.csrf { value -> value.disable() }
             .sessionManagement { config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
-            .authorizeHttpRequests { auth ->
-                auth
-                    .requestMatchers("/swagger-ui/**").permitAll()
-                    .requestMatchers("/swagger-ui.html/**").permitAll()
-                    .requestMatchers("/v3/**").permitAll()
-                    .anyRequest().authenticated()
-            }
             .addFilterBefore(
                 jwtFilter, UsernamePasswordAuthenticationFilter::class.java
             )
